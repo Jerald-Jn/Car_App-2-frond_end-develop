c5f30dde81907ebbd02c5c679b79b2b6d5e81b0d
Jerald-Jn <jjerald2000@gmail.com>
2025-09-01 06:36:19 +0530

    update-5

--- src/Component/checkout/CheckoutForm.tsx
+++ src/Component/checkout/CheckoutForm.tsx
@@ -20,7 +20,7 @@ export default function CheckoutForm() {
     const { error } = await stripe.confirmPayment({
       elements,
       confirmParams: {
-        return_url: "http://localhost:5173/payment-success",
+        return_url: "https://car-application-production.up.railway.app/payment-success",
       },
     });
 
